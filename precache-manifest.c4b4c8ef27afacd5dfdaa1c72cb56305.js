80fb9b9b50aa3a1c1087006283864e1a4853ecbf
Lucas Suarez <lucas.suarez.utepsa@gmail.com>
2018-11-28 11:08:35 -0400

    Updates

---
+++ precache-manifest.c4b4c8ef27afacd5dfdaa1c72cb56305.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7fc11064b45df326d40",
+    "url": "/crud-react/static/js/runtime~main.a7fc1106.js"
+  },
+  {
+    "revision": "fc728de496ff71bc2360",
+    "url": "/crud-react/static/js/main.fc728de4.chunk.js"
+  },
+  {
+    "revision": "5eff43b8f790f0fd4414",
+    "url": "/crud-react/static/js/1.5eff43b8.chunk.js"
+  },
+  {
+    "revision": "fc728de496ff71bc2360",
+    "url": "/crud-react/static/css/main.40c0a4e4.chunk.css"
+  },
+  {
+    "revision": "664aecdb21c9759b552349e5a8385bd5",
+    "url": "/crud-react/index.html"
+  }
+];
